9e5cc4eebc762e93185a3a18651a464c21c4bc51
STUDIOsoul <STUDIOsoul@users.noreply.github.com>
2025-07-28 15:41:28 +0100

    Update Model_Loader.py

--- Model_Loader.py
+++ Model_Loader.py
@@ -1,24 +1,22 @@
-from ..custom_node_base import SimpleNode
-
-class Model_Loader(SimpleNode):
+class Model_Loader:
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "model_path": ("STRING", {"default": "/content/ComfyUI/models/checkpoints/"}),
+                "text": ("STRING", {"default": "Hola desde Model_Loader!"})
             }
         }
 
     RETURN_TYPES = ("STRING",)
-    FUNCTION = "load_model"
+    FUNCTION = "output_text"
 
-    def load_model(self, model_path):
-        return (f"[Model Loader]: Loaded from {model_path}",)
+    def output_text(self, text):
+        return (f"[Model Loader]: {text}",)
 
 NODE_CLASS_MAPPINGS = {
     "Model_Loader": Model_Loader
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Model_Loader": "Model: Loader"
+    "Model_Loader": "Model Loader"
 }
