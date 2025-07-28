96ebcaa73fee19d2095fb5c1178cc82fae29e473
STUDIOsoul <STUDIOsoul@users.noreply.github.com>
2025-07-28 15:41:08 +0100

    Update Dataset_Loader.py

--- Dataset_Loader.py
+++ Dataset_Loader.py
@@ -1,24 +1,22 @@
-from ..custom_node_base import SimpleNode
-
-class Dataset_Loader(SimpleNode):
+class Dataset_Loader:
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "dataset_path": ("STRING", {"default": "/content/ComfyUI/dataset/"}),
+                "text": ("STRING", {"default": "Hola desde Dataset_Loader!"})
             }
         }
 
     RETURN_TYPES = ("STRING",)
-    FUNCTION = "load_dataset"
+    FUNCTION = "output_text"
 
-    def load_dataset(self, dataset_path):
-        return (f"[Dataset Loader]: Loaded from {dataset_path}",)
+    def output_text(self, text):
+        return (f"[Dataset Loader]: {text}",)
 
 NODE_CLASS_MAPPINGS = {
     "Dataset_Loader": Dataset_Loader
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Dataset_Loader": "Dataset: Loader"
+    "Dataset_Loader": "Dataset Loader"
 }
